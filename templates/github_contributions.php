d1ba4e1bc67b64698fa9a5209ff8c97de6c52072
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-11-23 13:21:34 +0100

    Fix link to github

--- templates/github_contributions.php
+++ templates/github_contributions.php
@@ -1,7 +1,7 @@
 <div class="panel panel-default">
 	<div class="panel-heading">
 		<h2>
-			<a href="">Recent contributions</a>
+			<a href="//github.com/ferrybig">Recent contributions</a>
 		</h2>
 	</div>
 	<div class="panel-body">
