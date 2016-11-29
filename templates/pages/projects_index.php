b290c69b3686d356f7c0a9f8999850e2629555b2
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-11-29 19:34:36 +0100

    Implement projects/index.html

--- templates/pages/projects_index.php
+++ templates/pages/projects_index.php
@@ -7,7 +7,61 @@
 				<h2>Projects</h2>
 			</div>
 			<div class="panel-body section-about-me">
-				This page is unsuported at this time... Check again soon...
+				<ul class="project-list media-list">
+					<?PHP if (file_exists(__DIR__ . "/../config/projects.json")): ?>
+						<?PHP $projects = useExpandSystem(json_decode(file_get_contents(__DIR__ . "/../config/projects.json"))); ?>
+					<?PHP else: ?>
+						<?PHP $projects = []; ?>
+					<?PHP endif; ?>
+					<?PHP
+					usort($projects, function($a, $b) {
+						return $a->created_at < $b->created_at ? 1 : -1;
+					});
+					?>
+					<?PHP foreach (limit($projects, 14) as $project): ?>
+						<li class="project media">
+							<?PHP if (isset($project->icons)): ?>
+								<div class="media-left">
+									<a href="<?= htmlentities($project->html_url) ?>">
+										<img class="media-object" src="<?= htmlentities($project->icons[0]->medium) ?>" alt="">
+									</a>
+								</div>
+							<?PHP endif; ?>
+							<div class="media-body">
+								<h3 class="media-heading project-name" style="font-size: 130%">
+									<a href="<?= htmlentities($project->html_url) ?>"><?= htmlentities($project->name) ?></a>
+								</h3>
+								<?PHP if (isset($project->language)): ?>
+									<p class="project-tags">
+										<small>
+											<?PHP if (is_array($project->language)): ?>
+												<?PHP foreach ($project->language as $language) : ?>
+													<span class="label language-tag language-tag-<?= htmlentities(strtolower($language)) ?>">
+														<?= htmlentities($language) ?>
+													</span>
+												<?PHP endforeach; ?>
+											<?PHP else: ?>
+												<span class="label language-tag language-tag-<?= htmlentities(strtolower($project->language)) ?>">
+													<?= htmlentities($project->language) ?>
+												</span>
+											<?PHP endif; ?>
+										</small>
+									</p>
+								<?PHP endif; ?>
+								<p class="project-time">
+									<small>
+										<time datetime="<?= htmlentities($project->created_at) ?>">
+											Created at: <?= htmlentities(gmdate('Y-m-d H:i:s', strtotime($project->created_at))) ?>
+										</time>
+									</small>
+								</p>
+								<p class="project-description">
+									<?= htmlentities(str_max_length($project->description, 255)) ?>
+								</p>
+							</div>
+						</li>
+					<?PHP endforeach; ?>
+				</ul>
 			</div>
 		</div>
 	</div>
