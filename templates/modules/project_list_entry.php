b06ab0ad82fd69a0fd9ec519b6809dc4061441f5
Fernando van Loenhout <ferrytinymailbox@gmail.com>
2016-12-01 13:15:29 +0100

    Reformat

--- templates/modules/project_list_entry.php
+++ templates/modules/project_list_entry.php
@@ -1,41 +1,41 @@
 <li class="project media">
-			<?PHP if(isset($project->icons)): ?>
-				<div class="media-left">
-					<a href="<?= htmlentities("projects/$project->slug.html") ?>">
-						<img class="media-object" src="<?= htmlentities($project->icons[0]->small) ?>" alt="" width="64" height="64">
-					</a>
-				</div>
-			<?PHP endif; ?>
-			<div class="media-body">
-				<h3 class="media-heading project-name" style="font-size: 130%">
-					<a href="<?= htmlentities("$project->slug.html") ?>"><?= htmlentities($project->nice_name) ?></a>
-				</h3>
-				<?PHP if(isset($project->language)): ?>
-					<p class="project-tags">
-						<small>
-							<?PHP if(is_array($project->language)): ?>
-								<?PHP foreach($project->language as $language) : ?>
-									<span class="label language-tag language-tag-<?= htmlentities(strtolower($language)) ?>">
-										<?= htmlentities($language) ?>
-									</span>
-								<?PHP endforeach; ?>
-							<?PHP else: ?>
-								<span class="label language-tag language-tag-<?= htmlentities(strtolower($project->language)) ?>">
-									<?= htmlentities($project->language) ?>
-								</span>
-							<?PHP endif; ?>
-						</small>
-					</p>
-				<?PHP endif; ?>
-				<p class="project-time">
-					<small>
-						<time datetime="<?= htmlentities($project->created_at) ?>">
-							Created at: <?= htmlentities(gmdate('Y-m-d H:i:s', strtotime($project->created_at))) ?>
-						</time>
-					</small>
-				</p>
-				<p class="project-description">
-					<?= htmlentities(str_max_length($project->description, 255)) ?>
-				</p>
-			</div>
-		</li>
+	<?PHP if(isset($project->icons)): ?>
+		<div class="media-left">
+			<a href="<?= htmlentities("projects/$project->slug.html") ?>">
+				<img class="media-object" src="<?= htmlentities($project->icons[0]->small) ?>" alt="" width="64" height="64">
+			</a>
+		</div>
+	<?PHP endif; ?>
+	<div class="media-body">
+		<h3 class="media-heading project-name" style="font-size: 130%">
+			<a href="<?= htmlentities("$project->slug.html") ?>"><?= htmlentities($project->nice_name) ?></a>
+		</h3>
+		<?PHP if(isset($project->language)): ?>
+			<p class="project-tags">
+				<small>
+					<?PHP if(is_array($project->language)): ?>
+						<?PHP foreach($project->language as $language) : ?>
+							<span class="label language-tag language-tag-<?= htmlentities(strtolower($language)) ?>">
+								<?= htmlentities($language) ?>
+							</span>
+						<?PHP endforeach; ?>
+					<?PHP else: ?>
+						<span class="label language-tag language-tag-<?= htmlentities(strtolower($project->language)) ?>">
+							<?= htmlentities($project->language) ?>
+						</span>
+					<?PHP endif; ?>
+				</small>
+			</p>
+		<?PHP endif; ?>
+		<p class="project-time">
+			<small>
+				<time datetime="<?= htmlentities($project->created_at) ?>">
+					Created at: <?= htmlentities(gmdate('Y-m-d H:i:s', strtotime($project->created_at))) ?>
+				</time>
+			</small>
+		</p>
+		<p class="project-description">
+			<?= htmlentities(str_max_length($project->description, 255)) ?>
+		</p>
+	</div>
+</li>
